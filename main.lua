5f92e7921ec44e468f36258a32b1ca25db01a987
Oskar <oskarro13@protonmail.com>
2026-05-17 14:57:14 +0200

    Fuel added!

--- main.lua
+++ main.lua
@@ -5,21 +5,47 @@ function love.load()
     player = {
         x = love.graphics.getWidth() / 2 - 60,
         y = love.graphics.getHeight() / 2 - 60,
-        spd = 250
+        spd = 250,
+        fuel = 100
     }
+    czcionka = love.graphics.newFont(32)
+    stan = "game"
 end
 
 function love.update(dt)
-    if love.keyboard.isDown("a") then player.x = player.x - player.spd * dt end
+    if stan == "game" then
+        if love.keyboard.isDown("a") then player.x = player.x - player.spd * dt end
+
+        if player.fuel <= 0 then
+            stan = "game-over"
+        end
+    end
+end
+
+function love.keypressed(key)
+    if stan == "game" then
+        if key == "a" then
+            player.fuel = player.fuel - 1
+        end
+    end
 end
 
 function love.draw()
     love.graphics.setColor(1, 1, 1)
+    love.graphics.setFont(czcionka)
     -- Gracz
-    love.graphics.rectangle("fill", player.x, player.y, 50, 50)
+    if stan == "game" then
+        love.graphics.rectangle("fill", player.x, player.y, 50, 50)
 
-    -- Podłoga
-    love.graphics.setColor(0, 0, 0)
-    love.graphics.rectangle("fill", 0, love.graphics.getHeight() - (love.graphics.getHeight() / 2 - 250),
-        love.graphics.getWidth(), love.graphics.getHeight() / 2 - 250, 0.5, 0.5)
+        -- Podłoga
+        love.graphics.setColor(0, 0, 0)
+        love.graphics.rectangle("fill", 0, love.graphics.getHeight() - (love.graphics.getHeight() / 2 - 250),
+            love.graphics.getWidth(), love.graphics.getHeight() / 2 - 250, 0.5, 0.5)
+
+        -- Paliwo
+        love.graphics.print("Fuel: " .. player.fuel, 10, 10)
+    end
+    if stan == "game-over" then
+        love.graphics.print("Game over!", love.graphics.getWidth() / 2 - 60, love.graphics.getHeight() / 2 - 60)
+    end
 end
